936aba565515418aa61cd75105f2cc8e11e054fd
AuroraLullaby <15350709254@163.com>
2023-10-15 17:48:18 +0800

    code

---
+++ src/server/redis/redis.cpp
@@ -0,0 +1,140 @@
+#include "redis.hpp"
+#include <iostream>
+using namespace std;
+
+Redis::Redis()
+    : _publish_context(nullptr), _subcribe_context(nullptr)
+{
+}
+
+Redis::~Redis()
+{
+    if (_publish_context != nullptr)
+    {
+        redisFree(_publish_context);
+    }
+
+    if (_subcribe_context != nullptr)
+    {
+        redisFree(_subcribe_context);
+    }
+}
+
+bool Redis::connect()
+{
+    // 负责publish发布消息的上下文连接
+    _publish_context = redisConnect("127.0.0.1", 6379);
+    if (nullptr == _publish_context)
+    {
+        cerr << "connect redis failed!" << endl;
+        return false;
+    }
+
+    // 负责subscribe订阅消息的上下文连接
+    _subcribe_context = redisConnect("127.0.0.1", 6379);
+    if (nullptr == _subcribe_context)
+    {
+        cerr << "connect redis failed!" << endl;
+        return false;
+    }
+
+    // 在单独的线程中，监听通道上的事件，有消息给业务层进行上报，因为subscribe是阻塞的，不xxxxx能因为订阅独占一个线程
+    thread t([&]() {
+        observer_channel_message();
+    });
+    t.detach();
+
+    cout << "connect redis-server success!" << endl;
+
+    return true;
+}
+
+// 向redis指定的通道channel发布消息  参数为：通道号 和 消息
+bool Redis::publish(int channel, string message)
+{
+    redisReply *reply = (redisReply *)redisCommand(_publish_context, "PUBLISH %d %s", channel, message.c_str());
+    if (nullptr == reply)
+    {
+        cerr << "publish command failed!" << endl;
+        return false;
+    }
+    freeReplyObject(reply);  // 发布消息后释放掉，方便下次绑定一个通道redisReply *reply
+    return true;
+}
+// 注意发布和订阅的区别因为publishCommand 不阻塞，发布消息后会立即回复
+//                 subscribe会阻塞，否则只能创建3个订阅通道（1个IO线程，3个worker线程），就都阻塞了，这也是同步发布机制的问题
+
+// 向redis指定的通道subscribe订阅消息
+bool Redis::subscribe(int channel)
+{
+    // SUBSCRIBE命令本身会造成线程阻塞等待通道里面发生消息，这里只做订阅通道，不接收通道消息
+    // 通道消息的接收专门在observer_channel_message函数中的独立线程中进行
+    // 只负责发送命令，不阻塞接收redis server响应消息，否则和notifyMsg线程抢占响应资源
+    if (REDIS_ERR == redisAppendCommand(this->_subcribe_context, "SUBSCRIBE %d", channel))
+    {
+        cerr << "subscribe command failed!" << endl;
+        return false;
+    }
+    // redisBufferWrite可以循环发送缓冲区，直到缓冲区数据发送完毕（done被置为1）
+    int done = 0;
+    while (!done)
+    {   // 把命令组装后写入缓存，再将缓存发送到redisserver , 也就是告诉redisserver，要订阅通道
+        // 这样就不用等待redisserver给响应了 redisGetReply的作用：以阻塞方式等待响应
+        if (REDIS_ERR == redisBufferWrite(this->_subcribe_context, &done))
+        {
+            cerr << "subscribe command failed!" << endl;
+            return false;
+        }
+    }
+    // redisCommand = redisAppendCommand + redisBufferWrite +redisGetReply
+    //   redisAppendCommand 把发送的命令缓存本地 
+    //  redisBufferWrite 从本地缓存发送到redisserver, 
+    // redisGetReply 等待响应结果，subscribe是阻塞等待，所以不能用这个函数 publish能用是因为执行后会立即响应
+
+    return true;
+}
+
+// 向redis指定的通道unsubscribe取消订阅消息
+bool Redis::unsubscribe(int channel)
+{
+    if (REDIS_ERR == redisAppendCommand(this->_subcribe_context, "UNSUBSCRIBE %d", channel))
+    {
+        cerr << "unsubscribe command failed!" << endl;
+        return false;
+    }
+    // redisBufferWrite可以循环发送缓冲区，直到缓冲区数据发送完毕（done被置为1）
+    int done = 0;
+    while (!done)
+    {
+        if (REDIS_ERR == redisBufferWrite(this->_subcribe_context, &done))
+        {
+            cerr << "unsubscribe command failed!" << endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+// 在独立线程中接收订阅通道中的消息
+void Redis::observer_channel_message()
+{
+    redisReply *reply = nullptr;  // 以专门启动一个线程以循环阻塞方式等待消息
+    while (REDIS_OK == redisGetReply(this->_subcribe_context, (void **)&reply)) // 循环等待
+    {
+        // 订阅收到的消息是一个带三元素的数组
+        if (reply != nullptr && reply->element[2] != nullptr && reply->element[2]->str != nullptr)
+        {
+            // 给业务层上报通道上发生的消息    0是通道消息  1是通道号 2 是通道传送的数据
+            _notify_message_handler(atoi(reply->element[1]->str) , reply->element[2]->str);
+        }
+
+        freeReplyObject(reply);
+    }
+
+    cerr << ">>>>>>>>>>>>> observer_channel_message quit <<<<<<<<<<<<<" << endl;
+}
+
+void Redis::init_notify_handler(function<void(int,string)> fn)
+{
+    this->_notify_message_handler = fn;
+} 
